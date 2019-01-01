06f6daa9017dbf09263230a2f6313f176bb6915c
sagar <sagar@192.168.100.14>
2019-01-01 03:13:13 -0800

    commit

---
+++ Adv_ java/src/nataraj_sir/Conn_test.java
@@ -0,0 +1,13 @@
+package nataraj_sir;
+import java.sql.*;
+public class Conn_test 
+{
+	public static void main(String args [])
+	throws Exception{
+		Connection con=DriverManager.getConnection
+				("jdbc:oracle:thin:@localhost:1521:orcl","system","sagar9027");
+		
+		
+	}
+
+}
