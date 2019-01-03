0958711f569824c3c4c49824e0963f62a2288999
sagar <sagar@192.168.100.7>
2019-01-03 11:12:44 -0800

    commit

---
+++ Adv_ java/src/nataraj_sir/SelectTest.java
@@ -0,0 +1,22 @@
+package nataraj_sir;
+import java.sql.*;
+import java.sql.DriverManager;
+
+public class SelectTest {
+	public static void main(String []args)
+	throws Exception{
+		//Establish database connection
+		Connection con=DriverManager.getConnection
+			("jdbc:oracle:thin:@localhost:1521:orcl","system","sagar9027");
+	//create jdbc statement object
+		Statement st=con.createStatement();
+		//execute query and set result in result set object
+	 ResultSet rs=st.executeQuery("SELECT * FROM bOOK16");
+	 while(rs.next()!=false)
+	 {
+		 System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));
+	 }
+	 
+	}
+
+}
